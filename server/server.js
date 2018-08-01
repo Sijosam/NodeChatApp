58ca948cbb4869a2caf46ec2452b9cb4059d505b
Sijo Sam <sijosam33@gmail.com>
2018-08-01 17:36:19 +0530

    2rd push io

--- server/server.js
+++ server/server.js
@@ -1,25 +1,32 @@
 const path     = require('path');
+const http     = require('http');
 const express  = require('express');
+const socketIO = require('socket.io');
 
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.port || 3000;
 var app = express();
-
+var server = http.createServer(app);
+var io = socketIO(server);
 
 
 
 app.use(express.static(publicPath));
 
 
+io.on('connection', (socket) => {
+  console.log('new user Connected');
 
 
 
+  socket.on('disconnect', (socket) => {
+  console.log('client has leaved');
+});
 
+});
 
-
-
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('Started the Server');
 });
+// Rijo.Sam@cognizant.com
